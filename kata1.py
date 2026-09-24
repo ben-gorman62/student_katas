b990946708af8808b87f023e368e3405bbbfb3a0
Ben Gorman <thebengorman@icloud.com>
2026-09-24 11:04:25 +0100

    Kata1 complete

--- kata1.py
+++ kata1.py
@@ -4,4 +4,24 @@
 # 
 # print out all the capital letters from that string.
 
+# decompose time
+# make an empty string
+# make a loop through each index in sentence
+# check if character in index is uppercase
+# add uppercase index to empty string
+
+
+# use string.findall("([A_Z])")
+
+
+
 our_string = "Here is a string Paul wrote. It has CAPITAL letters in it... Wow."
+def find_caps(string):
+    caps_string = ""
+    for char in string:
+        if char.isupper():
+            caps_string += char
+            
+    return caps_string
+
+print(find_caps(our_string))
